c16e0013639463d0043cdc4710fd8df119124de1
Paolo <pascuccip9@gmail.com>
2022-01-14 16:20:11 +0100

    Add flights table

---
+++ database/migrations/2022_01_14_151757_create_flights_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination');
+            $table->string('dep_data');
+            $table->string('departure');
+            $table->string('arr_data');
+            $table->string('arrival');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+}
