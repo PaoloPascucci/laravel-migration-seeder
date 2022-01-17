62206a07332839510aa8d515fb827b73a9a0ef24
Paolo <pascuccip9@gmail.com>
2022-01-17 15:59:07 +0100

    add flightseeder adjustment and complete flight exercise

--- database/seeds/FlightSeeder.php
+++ database/seeds/FlightSeeder.php
@@ -9,17 +9,18 @@ class FlightSeeder extends Seeder
     /**
      * Run the database seeds.
      *
-     * @return void
+     * return void
      */
     public function run(Faker $faker)
     {
         for ($i=0; $i < 10; $i++) { 
             $_flights = new flight(); 
-            $_flights->destination = $faker;
-            $_flights->departure = $faker;
-            $_flights->arrival = $faker;
-            $_flights->dep_data = $faker;
-            $_flights->arr_data = $faker;
+            $_flights->destination = $faker->word();
+            $_flights->departure = $faker->city();
+            $_flights->arrival = $faker->city();
+            $_flights->dep_data = $faker->dateTime();
+            $_flights->arr_data = $faker->dateTime();
+            $_flights->price = $faker->numberBetween(0, 100);
             $_flights->save();
         }
 
